f18b98f9400996e9f6108310c082f17cb732a7a4
Desiree_McElroy <desireeashley90@gmail.com>
2021-05-14 12:02:27 -0700

    recorrecting functions

--- acquire.py
+++ acquire.py
@@ -11,47 +11,92 @@ from pydataset import data
 
 
 
-# **************** TITANIC DATA *****************
-def get_titanic_data(cached = False):
+# Create helper function to get the necessary connection url.
+
+def get_connection(db_name):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    from env import host, username, password
+    return f'mysql+pymysql://{username}:{password}@{host}/{db_name}'
+
+# Use the above helper function and a sql query in a single function.
+def get_new_titanic_data():
     '''
     This function reads in the titanic data from the Codeup db
     and returns a pandas DataFrame with all columns.
     '''
-    if cached == False or os.path.isfile('titanic_df.csv') == False:
-        query = '''
-        SELECT * 
-        FROM passengers;
-        '''
-        titanic_df = pd.read_sql(query, get_db_url('titanic_db'))
-        #also cache the data we read from the db, to a file on disk
-        titanic_df.to_csv('titanic_df.csv')
-    else:
-        #either the cached parameter was true, or a file exists on disk. Read that into a df instead of going to the database
-        titanic_df = pd.read_csv('titanic_df.csv', index_col=0)
+    sql_query = 'SELECT * FROM passengers'
+    return pd.read_sql(sql_query, get_connection('titanic_db'))
 
-    #return our dataframe regardless of its origin
-    return titanic_df
+# Use helper function and sql query 
 
-# ******************* IRIS DATA *********************
-def get_iris_data(cached = False):
+def get_new_iris_data():
     '''
     This function reads in the iris data from the Codeup db
     and returns a pandas DataFrame with all columns.
     '''
-    if cached == False or os.path.isfile('iris_df.csv') == False:
-        query = '''
-        SELECT * 
-        FROM measurements
-        JOIN species USING (species_id);;
-        '''
-        iris_df = pd.read_sql(query, get_db_url('iris_db'))
-        # cache it as a csv file
-        iris_df.to_csv('iris_df.csv')
+    sql_query = '''
+    SELECT *
+    FROM measurements
+    JOIN species USING (species_id)
+    '''
+    return pd.read_sql(sql_query, get_connection('iris_db'))
 
-    else: # if cached parameter is True or file exists on disk, read the file into a pandas df
-        iris_df = pd.read_csv('iris_df', index_col=0)
-     # return the iris df regardless of origin
-    return iris_df
+
+
+
+# **************** TITANIC DATA *****************
+
+def get_titanic_data(cached=False):
+    '''
+    This function reads in titanic data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in titanic df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('titanic_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = get_new_titanic_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('titanic_df.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('titanic_df.csv', index_col=0)
+        
+    return df
+
+
+# ******************* IRIS DATA *********************
+
+def get_iris_data(cached=False):
+    '''
+    This function reads in titanic data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in titanic df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('titanic_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = get_new_iris_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('iris_df.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('iris_df.csv', index_col=0)
+        
+    return df
+
+
+
+
 
 
 
