6d899d13794872abc845f5d69ae66087b8298767
Desiree_McElroy <desireeashley90@gmail.com>
2021-05-14 11:53:44 -0700

    correcting function

--- acquire.py
+++ acquire.py
@@ -12,39 +12,11 @@ from pydataset import data
 
 
 # **************** TITANIC DATA *****************
-def get_titanic_date():
+def get_titanic_data(cached = False):
     '''
     This function reads in the titanic data from the Codeup db
     and returns a pandas DataFrame with all columns.
     '''
-    sql_query = 'SELECT * FROM passengers'
-    return pd.read_sql(sql_query, get_connection('titanic_db'))
-
-
-
-# ******************* IRIS DATA *********************
-def get_iris_data():
-    '''
-    This function reads in the iris data from the Codeup db
-    and returns a pandas DataFrame with all columns.
-    '''
-    sql_query = '''
-    SELECT *
-    FROM measurements
-    JOIN species USING (species_id)
-    '''
-    return pd.read_sql(sql_query, get_connection('iris_db'))
-
-
-
-
-def get_titanic_data(cached=False):
-    '''
-    This function reads in titanic data from Codeup database and writes data to
-    a csv file if cached == False or if cached == True reads in titanic df from
-    a csv file, returns df.
-    '''
-   #If the cached parameter is false, or the csv file is not on disk, read from the database into a dataframe
     if cached == False or os.path.isfile('titanic_df.csv') == False:
         query = '''
         SELECT * 
@@ -60,15 +32,12 @@ def get_titanic_data(cached=False):
     #return our dataframe regardless of its origin
     return titanic_df
 
-
-
-def get_iris_data(cached=False):
+# ******************* IRIS DATA *********************
+def get_iris_data(cached = False):
     '''
-    This function reads in titanic data from Codeup database and writes data to
-    a csv file if cached == False or if cached == True reads in titanic df from
-    a csv file, returns df.
+    This function reads in the iris data from the Codeup db
+    and returns a pandas DataFrame with all columns.
     '''
-    # read the db from codeup db into a df if the cached parameter is false or the file is not on disk
     if cached == False or os.path.isfile('iris_df.csv') == False:
         query = '''
         SELECT * 
@@ -83,3 +52,7 @@ def get_iris_data(cached=False):
         iris_df = pd.read_csv('iris_df', index_col=0)
      # return the iris df regardless of origin
     return iris_df
+
+
+
+
