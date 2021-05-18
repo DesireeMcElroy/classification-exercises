55d8634e69013cec5bbfba73917f3efbb0fd063f
Desiree_McElroy <desireeashley90@gmail.com>
2021-05-18 10:12:22 -0700

    adding titanic function

--- prepare.py
+++ prepare.py
@@ -9,3 +9,17 @@ def prep_iris(iris_df):
     dummy_df = pd.get_dummies(iris_df[['species']])
     iris_df = pd.concat([df, dummy_df], axis=1)
     return iris_df
+
+
+
+def train_validate_test_split(df, seed=123):
+    train_and_validate, test = train_test_split(
+        df, test_size=0.2, random_state=seed, stratify=df.survived
+    )
+    train, validate = train_test_split(
+        train_and_validate,
+        test_size=0.3,
+        random_state=seed,
+        stratify=train_and_validate.survived,
+    )
+    return train, validate, test
