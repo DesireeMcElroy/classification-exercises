1c99503c5b65e38b8b24c0b21487b5ad958bb0f8
Desiree_McElroy <desireeashley90@gmail.com>
2021-05-18 21:00:16 -0700

    correcting imports

--- prepare.py
+++ prepare.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 import acquire
+from sklearn.model_selection import train_test_split
 
 
 def prep_iris(iris_df):
     iris_df = iris_df.drop(columns=['species_id', 'measurement_id'])
     iris_df = iris_df.rename(columns={'species_name': 'species'})
     dummy_df = pd.get_dummies(iris_df[['species']])
-    iris_df = pd.concat([df, dummy_df], axis=1)
+    iris_df = pd.concat([iris_df, dummy_df], axis=1)
     return iris_df
 
 
